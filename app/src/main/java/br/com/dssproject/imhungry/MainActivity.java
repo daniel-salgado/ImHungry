829d01be30be576a1f4e6ebb1ec7008f391d27d1
Daniel Salgado <daniel@salgadobr.com>
2017-11-06 21:54:38 +1300

    New logo and splashscreen

--- app/src/main/java/br/com/dssproject/imhungry/MainActivity.java
+++ app/src/main/java/br/com/dssproject/imhungry/MainActivity.java
@@ -1,31 +1,55 @@
 package br.com.dssproject.imhungry;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 
+
+//Animations --> http://awsrh.blogspot.co.nz/2017/10/welcome-screen-with-uptodown-animation.html
 public class MainActivity extends AppCompatActivity {
 
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    LinearLayout linearTop;
+    LinearLayout linearBottom;
+
+
+    Animation uptodown;
+    Animation downtoup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        linearTop = (LinearLayout) findViewById(R.id.linearTop);
+        linearBottom = (LinearLayout) findViewById(R.id.linearBottom);
+        uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
+        downtoup = AnimationUtils.loadAnimation(this,R.anim.downtoup);
+        linearTop.setAnimation(uptodown);
+        linearBottom.setAnimation(downtoup);
+
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent intent = new Intent(MainActivity.this,FoodListActivity.class);
+                startActivity(intent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+
     }
 
-    public void btnHungry(View view) {
-
-        Toast.makeText(MainActivity.this,"Hey", Toast.LENGTH_SHORT).show();
-
-        Intent intent = new Intent(this, FoodListActivity.class);
-
-
-
-        startActivity(intent);
-
-    }
 
 }
